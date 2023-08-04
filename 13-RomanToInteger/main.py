de591b1189145d07446a1be6ef73a2b77628aa0b
kevin.zingg <kevin.zingg@siemens.com>
2023-08-04 10:13:03 +0200

    romanToInt (done)

--- 13-RomanToInteger/main.py
+++ 13-RomanToInteger/main.py
@@ -21,26 +21,37 @@ Result = 58
 
 
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def __init__(self):
+        self.s = 'LV'  # Define s as an instance variable with value 'L'
 
+    def romanToInt(self, n: str) -> int:  # Remove s parameter, since it's an instance variable
+        print('roman to int')
+        self.s = n
         letter_to_number_mapping = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
-}
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
 
-        s = 'L'
-            
-        result = letter_to_number_mapping[s]
+        total = 0
+        previous_value = 0
 
-        return result
-    
-solution = Solution()
+        for letter in reversed(self.s):
+                value = letter_to_number_mapping[letter]
+                if value < previous_value:
+                     total -= value
+                else:
+                     total += value
+                     previous_value = value
+        return total
 
-result = solution.romanToInt('L')
+s = "LVIII"
+so = Solution()
 
-print(result)
+i = so.romanToInt(s)
+
+print(f'{i}')

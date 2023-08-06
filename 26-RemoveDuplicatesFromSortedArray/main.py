0c2db523a09a2a7d5e408917f0f0a020a5c9974b
Kevin Zingg <56632548+KevinZingg@users.noreply.github.com>
2023-08-06 14:14:59 +0200

    removeDuplicateFromSortedArray(✅)

--- 26-RemoveDuplicatesFromSortedArray/main.py
+++ 26-RemoveDuplicatesFromSortedArray/main.py
@@ -1,19 +1,22 @@
-'''
-
-Itterate
-
-'''
-
 from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if len(set(nums)) < len(nums):
-            print("There's a dupe!")
+        if not nums:  # handle empty list
+            return 0
 
+        idx = 0  # a pointer to track the position in the original list
 
-nums = [1, 1, 2]
+        # Loop through the list and place unique elements at the beginning
+        for num in nums:
+            if idx == 0 or nums[idx - 1] != num:
+                nums[idx] = num
+                idx += 1
 
+        # The unique numbers are in the list nums[:idx]
+        return idx  # Return the number of unique elements
+
+nums = [0, 1, 1, 1, 2, 3, 4, 5, 5, 5, 6, 6]
 solution = Solution()
-
-solution = solution.removeDuplicates(nums)
+length = solution.removeDuplicates(nums)
+print(nums[:length])  # Output will be [0, 1, 2, 3, 4, 5, 6]
---
+++ 26-RemoveDuplicatesFromSortedArray/test.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+
+        for item in nums:
+
+            print = next(item)
+            print(print)
+
+nums = [0,1,1,1,2,3,4,5,5,5,6,6]
+
+solution = Solution()
+
+solution = solution.removeDuplicates(nums)

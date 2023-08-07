1b9e8fa88d31ecaa6b5a293caeba817a1afa5a3e
kevin.zingg <kevin.zingg@siemens.com>
2023-08-07 10:24:47 +0200

    faultyKeyboard (🚧)

---
+++ 2810-FaultyKeyboard/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+
+        temp_list = []
+        temp_s = ""
+
+        for letter in s:
+            if letter != "i":
+                temp_list.append(letter)
+            elif letter == "i":
+                temp_list = temp_list[::-1]
+
+        s = temp_s.join(temp_list)
+        return s
+
+s = "string"
+
+solution = Solution()
+
+solution.finalString(s)

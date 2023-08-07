49083e8f2889dbba1853f1bfd2d1ee33983191e1
kevin.zingg <kevin.zingg@siemens.com>
2023-08-07 13:21:07 +0200

    fizzBuzzList (✅)

--- 412-FizzBuzz/main.py
+++ 412-FizzBuzz/main.py
@@ -2,34 +2,22 @@ from typing import List
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-
         ToBeFilledList = []
-
         FizzBuzzList = [i for i in range(1, n + 1)]
-
-        print(FizzBuzzList)
-
         for position in FizzBuzzList:
-            print(position)
             ToBeFilledList.append(position)
             if position % 3 == 0 and position % 5 == 0:
-                print("FizzBuzz")
                 ToBeFilledList.pop(position-1)
                 ToBeFilledList.insert(position, "FizzBuzz")
-                return ToBeFilledList
             elif position % 5 == 0:
-                print("Buzz")
                 ToBeFilledList.pop(position-1)
                 ToBeFilledList.insert(position, "Buzz")
             elif position % 3 == 0:
-                print("Fizz")
-                print(position)
                 ToBeFilledList.pop(position-1)
                 ToBeFilledList.insert(position, "Fizz")
-
-                print(ToBeFilledList)
-
-
+            n = ToBeFilledList
+            n = [str(item) for item in n]
+        return n
 
 
 n = 15

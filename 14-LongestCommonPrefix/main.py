c560865b88f23bb5f54a413a04995bef695ebf80
kevin.zingg <kevin.zingg@siemens.com>
2023-08-08 11:11:58 +0200

    longestCommonPrefix (✅)

--- 14-LongestCommonPrefix/main.py
+++ 14-LongestCommonPrefix/main.py
@@ -1,59 +1,23 @@
-'''
-Array of words -> Find out until when their letters are the same
-
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
-
-Separate letters of each word
-
-flower = f, l, o, w, e, r
-flow = f, l, o, w
-...
-
-Take the longest word.
-
-take each letter and compare it to the other letters
-
-f-l-o-w-e-r
-T-T-F-F-F-F (True = Same Letter / F = Not Same Letter)
-f-l-i-g-h-t
-
-
-B-B-A-A-A-B-B
-T-F-F-T-T-F-F (True = Same Letter / F = Not Same Letter)
-B-A-B-A-A-B-A
-
-print longest segment of true letters:
-
-B-B-A-A-A-B-B (Word)
-T-F-F-T-T-F-F (True = Same Letter / F = Not Same Letter)
-B-A-B-A-A-B-A (Word)
-
-
-
-'''
 from typing import List
+import re
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs = ["flower","flow","flight"] #["dog","racecar","car"]
-        first_word = strs[0]
-        was_printed = 0
+        if not strs:
+            return "There is no common prefix"
 
-        for first_word_letter in first_word:
-            amount_of_items = 0
+        strs.sort()
 
-            for word in strs:
-                amount_of_items += 1
+        first = strs[0]
+        last = strs[-1]
 
-                for letter in word:
-                    if letter == first_word_letter and amount_of_items == len(strs):
-                        was_printed += 1
-                        print(letter)
+        i = 0
 
-        print(strs)
-        if was_printed == 0:
-            print("nu uh")
+        while i < len(first) and i < len(last) and first[i] == last[i]:
+            i += 1
+        return first[:i]
 
+
+strs = ["flower", "flow", "flight"] #["dog","racecar","car"]
 
 solution = Solution()
 

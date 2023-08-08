bc1ac48ec248b73befd8256e3d9723dca5c8f312
kevin.zingg <kevin.zingg@siemens.com>
2023-08-08 11:22:15 +0200

    longestCommonPrefix (✅)

--- 14-LongestCommonPrefix/main.py
+++ 14-LongestCommonPrefix/main.py
@@ -4,21 +4,14 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return "There is no common prefix"
-
         strs.sort()
-
         first = strs[0]
         last = strs[-1]
-
         i = 0
-
         while i < len(first) and i < len(last) and first[i] == last[i]:
             i += 1
         return first[:i]
 
-
 strs = ["flower", "flow", "flight"] #["dog","racecar","car"]
-
 solution = Solution()
-
 solution.longestCommonPrefix([])

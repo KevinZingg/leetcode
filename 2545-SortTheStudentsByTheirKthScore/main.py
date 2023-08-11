73f62e68be03864ce328c3667150a6b7b2b6d5af
kevin.zingg <kevin.zingg@siemens.com>
2023-08-11 15:50:33 +0200

    sortTheSudentByTheirKthScore (🚧)

---
+++ 2545-SortTheStudentsByTheirKthScore/main.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        print("Sol")
+
+
+
+
+
+score = [[10, 6, 9, 1], [7, 5, 11, 2], [4, 8, 3, 15]]
+k = 2
+
+solution = Solution()
+
+solution.sortTheStudents(score, k)

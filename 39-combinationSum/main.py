9cce645e2973f684c65fba96e6bbde52e7e98808
kevin.zingg <kevin.zingg@siemens.com>
2023-08-17 08:45:25 +0200

    combinationSum (🚧)

---
+++ 39-combinationSum/main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        sol_list = []
+
+
+
+
+candidates = [2,3,6,7]
+target = 7
+
+solution = Solution()
+solution.combinationSum(candidates, target)
